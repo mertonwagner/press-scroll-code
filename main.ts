2fe4c46726696c3bb5213db5cb7cf9b378f8ff74
mertonwagner <107504603+mertonwagner@users.noreply.github.com>
2022-06-15 10:47:42 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,15 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Ghost)
 })
 basic.forever(function () {
-    if (input.lightLevel() < 128) {
-    	
+    if (input.lightLevel() > 128) {
+        basic.showString("Hello!")
+    } else {
+        images.createBigImage(`
+            # . . . . . . . . #
+            . # . . . . . . # .
+            . . # . . . . # . .
+            . . . # . . # . . .
+            . . . . # # . . . .
+            `).scrollImage(1, 200)
     }
 })
